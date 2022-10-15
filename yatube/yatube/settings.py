e2814d7101f6fc2baecd71a20d1607f343602cb7
Andrey Zharov <vin999_09@mail.ru>
2022-10-15 15:48:10 +0300

    Update: Final

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -7,10 +7,8 @@ SECRET_KEY = 'django-insecure-x1gexxaa$!6$mzx_nh2yr97h#^21433zfc@tbuzmar-!#^90no
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '[::1]',
-    'testserver',
+    'www.kotobormot.pythonanywhere.com',
+    'kotobormot.pythonanywhere.com',
 ]
 
 INSTALLED_APPS = [
@@ -25,6 +23,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -35,6 +34,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
